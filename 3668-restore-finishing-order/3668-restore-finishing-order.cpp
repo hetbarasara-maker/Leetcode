462e8a7867ddc5c9640fc888d1fde8069a5fb75b
hetbarasara-maker <hetbarasara@gmail.com>
2026-09-23 15:25:45 +0530

    Time: 3 ms (30.92%), Space: 50.6 MB (24.02%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int> st(friends.begin(), friends.end());
+        vector<int> ans;
+
+        for(int i=0;i<order.size();i++){
+            if(st.count(order[i])) {
+                ans.push_back(order[i]);
+            }
+        }
+        return ans;
+    }
+};
